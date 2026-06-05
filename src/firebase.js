b169772e231f2cc2e1884486628da66064569d49
Nayomi Arvell <nayomi.arvell@gmail.com>
2026-06-05 10:24:31 +0200

    adds env variables

--- src/firebase.js
+++ src/firebase.js
@@ -2,12 +2,12 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBSXzKgTAZL0A2hb1mcOoS-iEZB0Ot55iw",
-  authDomain: "rideandread-876f9.firebaseapp.com",
-  projectId: "rideandread-876f9",
-  storageBucket: "rideandread-876f9.firebasestorage.app",
-  messagingSenderId: "1068340167829",
-  appId: "1:1068340167829:web:3be96344c00a3006e6c79c"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID
 };
 
 const app = initializeApp(firebaseConfig);
